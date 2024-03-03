00b6e7b931f5dc494d57e2f29601899ef8e0b798
zori13 <zori.gon@mail.com>
2024-03-03 19:32:03 +0100

    Actualizar y añadir configración de wagmi

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
 import { AppLayout } from './components/ui/layouts'
 import { Home } from './components/pages'
+import { WagmiConfig } from 'wagmi'
+import { ConnectKitProvider}from 'connectkit'
+import { config } from './config/wagmi'
+
 
 function App(){
     return(
-       <AppLayout> 
-          <Home/>       
-       </AppLayout> 
+      <WagmiConfig config = {config}>
+         <ConnectKitProvider>
+            <AppLayout> 
+               <Home/>  
+                  
+            </AppLayout> 
+         </ConnectKitProvider> 
+      </WagmiConfig>
     )
 }
 export default App  
