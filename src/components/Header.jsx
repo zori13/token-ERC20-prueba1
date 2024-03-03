8f99296f07b0c6c40209d96a29fc8ab103efcc70
zori13 <zori.gon@mail.com>
2024-03-03 19:32:40 +0100

    Añadir botón conexión wallet de connectkit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,4 @@
+import { ConnectKitButton } from 'connectkit'
 export default function Header(){
     return(
      <header className="border-b shadow py-2 px-3 flex justify-between items-center">        
@@ -5,7 +6,7 @@ export default function Header(){
         className="sm:hidden" width={47}/>
         <img src="/estrella-p.png" alt= "estrella token"
         className="hidden sm:block" width={300} />
-        <button className="bg-gray rounded-xl px-2 text-sm py-2">Wallet</button>        
+        <ConnectKitButton showBalance/>       
      </header>
     )  
 }
